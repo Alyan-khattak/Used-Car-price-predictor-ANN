af1a88aef3a9805c694f9664ac923271fe4c807a
Alyan-khattak <alyankhattake@gmail.com>
2026-09-05 10:56:36 +0500

    Data Validation Artifacts

--- carprice/entity/artifact_entity.py
+++ carprice/entity/artifact_entity.py
@@ -23,3 +23,31 @@ class DataIngestionArtifact:
 
     test_file_path: str
     # "Artifacts/timestamp/data_ingestion/ingested/test.csv"
+
+
+
+
+# ── ARTIFACT 2: DataValidationArtifact ───────────────────────────
+# DataValidation.initiate_data_validation() return karta hai
+# DataTransformation ko milega
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    # True = valid → pipeline continue
+    # False = invalid → pipeline flag
+
+    valid_train_file_path: str
+    # "Artifacts/timestamp/data_validation/validated/train.csv"
+
+    valid_test_file_path: str
+    # "Artifacts/timestamp/data_validation/validated/test.csv"
+
+    invalid_train_file_path: str
+    # "Artifacts/timestamp/data_validation/invalid/train.csv"
+
+    invalid_test_file_path: str
+    # "Artifacts/timestamp/data_validation/invalid/test.csv"
+
+    drift_report_file_path: str
+    # "Artifacts/timestamp/data_validation/drift_report/report.yaml"
+
